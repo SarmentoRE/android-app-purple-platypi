ef5bc1800c4e61dd0f16e745c7f353becd503b42
Shane Gifford <giffordsm@vcu.edu>
2017-10-07 10:41:02 -0400

    Adding in Search Functionality (broken)

--- app/src/main/java/cmsc355/contactapp/Contact.java
+++ app/src/main/java/cmsc355/contactapp/Contact.java
@@ -5,4 +5,18 @@ public class Contact {
     public String address;
     public String phoneNumber;
     public String email;
+
+    public Contact() {
+        name = "N/A";
+        address = "N/A";
+        phoneNumber = "N/A";
+        email = "N/A";
+    }
+
+    public Contact(String n, String a, String p, String e) {
+        name = n;
+        address = a;
+        phoneNumber = p;
+        email = e;
+    }
 }
--- app/src/main/java/cmsc355/contactapp/ContactsActivity.java
+++ app/src/main/java/cmsc355/contactapp/ContactsActivity.java
@@ -1,11 +1,21 @@
 package cmsc355.contactapp;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ContactsActivity extends AppCompatActivity {
 
@@ -15,6 +25,7 @@ public class ContactsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contacts);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -25,5 +36,22 @@ public class ContactsActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
     }
 }
