9a4c532c58e4585b3138a86fcccc377359f176bc
Shane Gifford <giffordsm@vcu.edu>
2017-10-09 13:05:58 -0400

    adding search bar (nonfunctional), removing EditGroupActivity

--- app/src/main/java/cmsc355/contactapp/GroupsActivity.java
+++ app/src/main/java/cmsc355/contactapp/GroupsActivity.java
@@ -1,10 +1,15 @@
 package cmsc355.contactapp;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 public class GroupsActivity extends AppCompatActivity {
@@ -28,6 +33,20 @@ public class GroupsActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
     }
-}
 
-//TODO - each group clicked should open EditGroupActivity with that group's info loaded
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        // Inflate menu to add items to action bar if it is present.
+        inflater.inflate(R.menu.menu_search, menu);
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+}
