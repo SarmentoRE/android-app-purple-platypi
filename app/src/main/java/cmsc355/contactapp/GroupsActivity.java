8cd332e52a62d17a78846f23a1ec92b654e0576b
Austin Sarmento <austins0692@gmail.com>
2017-10-14 19:38:29 -0400

    made random group work and fixed a problem with my last commit (sorry for broken commit to master)

--- app/src/main/java/cmsc355/contactapp/GroupsActivity.java
+++ app/src/main/java/cmsc355/contactapp/GroupsActivity.java
@@ -1,9 +1,15 @@
 package cmsc355.contactapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -20,5 +26,31 @@ public class GroupsActivity extends AppCompatActivity {
         ContactGroup.GenerateRandomGroups(groupsList, 3);       //TODO - replace this line with pulling groups from database
 
         recyclerView.setAdapter(new GroupsAdapter(groupsList));
+    }
+
+    public ArrayList<JSONObject> getContactList(SQLiteDatabase db) {
+        String table = "Group";
+        String[] columns = {"JSON"};
+        String selection = DatabaseContract.Contact.COLUMN_JSON + " = ?";
+        String[] selectionArgs = null;
+        String groupBy = null;
+        String having = null;
+        String orderBy = null;
+
+        Cursor cursor = db.query(table, columns, selection, selectionArgs, groupBy, having, orderBy);
+
+        JSONObject json = new JSONObject();
+        ArrayList contactsJSON = new ArrayList();
+
+        while (cursor.moveToNext()) {
+            String stringForm = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseContract.Contact.COLUMN_JSON));
+            try {
+                json = new JSONObject(stringForm);
+            } catch (JSONException e) {
+                Log.d("Contacts Activity", "Could not parse JSON: **" + stringForm + "**");
+            }
+            contactsJSON.add(json);
+        }
+        return contactsJSON;
     }
 }
