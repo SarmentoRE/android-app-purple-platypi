ebbfec575018f5d0ef58caf616ea256a9f464486
Trez2 <reznikovt@vcu.edu>
2017-10-18 22:05:49 -0400

    Added check to see if NFC is enabled on the device. If yes, display "Good to go", if no, take user to NFC settings page

--- app/src/main/java/cmsc355/contactapp/ConnectActivity.java
+++ app/src/main/java/cmsc355/contactapp/ConnectActivity.java
@@ -1,23 +1,25 @@
 package cmsc355.contactapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcManager;
+import android.os.Bundle;
 import android.support.v4.util.ArrayMap;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import static cmsc355.contactapp.Contact.contactsMock;
+import static cmsc355.contactapp.App.context;
 import static cmsc355.contactapp.Contact.myInfoMock;
 
 public class ConnectActivity extends AppCompatActivity {
     // This is a test comment
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +33,45 @@ public class ConnectActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         ArrayMap<String, Object> myInfoAttributes = Utilities.JSONToMap(myInfoMock.getAttributes());
         recyclerView.setAdapter(new ConnectAdapter(myInfoAttributes));
-    }
 
+
+        nfcCheck(); // check for NFC connection
+    /* Todo see what here isn't working and why
+        Intent nfcIntent = new Intent(this, getClass()); // Instantiate the intent to NFC connect
+        nfcIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP); // add flag, pops the activity to the forefront
+
+        nfcPendingIntent =
+        PendingIntent.getActivity(this, 0, nfcIntent, 0); // sets a pending intent
+
+        IntentFilter tagIntentFilter =
+        new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED); //prepares the app to accept the NFC NDEF when its discovered
+        try {
+            tagIntentFilter.addDataType("text/plain");
+            intentFiltersArray = new IntentFilter[]{tagIntentFilter};
+        }
+        catch (Throwable t) {
+            t.printStackTrace();
+        }
+    */
+    }
+    public void nfcCheck (){
+
+        NfcManager manager = (NfcManager) context.getSystemService(Context.NFC_SERVICE); // This gets the manager object instantiated, and sets it to the NFC service
+        NfcAdapter adapter = manager.getDefaultAdapter(); // this instantiates the adapter and sets it to the default
+
+        if (adapter != null && adapter.isEnabled()) {
+            /* If adapter is null or adapter is enabled
+            *  if adapter is null, then the device does not support NFC, however I felt that it was unnecessary to make it a separate elseif statement
+            *  since it is included in the android manifest "uses Feature"
+            */
+            Toast.makeText(getApplicationContext(), "NFC is on, you're good to go!",
+                    Toast.LENGTH_SHORT).show(); //Show toast that NFC is on
+        }else{
+            Toast.makeText(getApplicationContext(), "NFC must be turned on by User in Settings",
+                    Toast.LENGTH_LONG).show(); //Show toast that NFC is off
+            startActivity(new Intent(android.provider.Settings.ACTION_NFC_SETTINGS)); //open settings page for NFC
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_default,menu);
@@ -52,4 +91,23 @@ public class ConnectActivity extends AppCompatActivity {
 
         }
     }
+    /* todo figure out how to make the below methods correctly listen for and stop listening for the NDEF tag
+    @Override
+    protected void onResume() {
+        super.onResume();
+        nfcAdpt.enableForegroundDispatch(
+        this,
+        nfcPendingIntent,
+        intentFiltersArray,
+        null);
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        nfcAdpt.disableForegroundDispatch(this);
+    }
+    */
+
 }
