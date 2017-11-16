f2882f139a2ff74c9bb7b745b31146d5148af919
Trez2 <reznikovt@vcu.edu>
2017-11-15 19:40:52 -0500

    Adjusted NFC Functionality

--- app/src/main/java/com/cmsc355/contactapp/ConnectActivity.java
+++ app/src/main/java/com/cmsc355/contactapp/ConnectActivity.java
@@ -14,7 +14,6 @@ import android.nfc.NfcEvent;
 import android.nfc.NfcManager;
 import android.nfc.Tag;
 import android.nfc.tech.Ndef;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.util.ArrayMap;
@@ -28,11 +27,8 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
-
+import static android.widget.Toast.makeText;
 import static com.cmsc355.contactapp.App.context;
-import static com.cmsc355.contactapp.ConnectActivity.TAG;
 
 public class ConnectActivity extends NonHomeActivity {
 
@@ -75,6 +71,7 @@ public class ConnectActivity extends NonHomeActivity {
         }
         */
     }
+
     // Handles the incoming Intent
     private void handleIntent(Intent intent) {
         String action = intent.getAction();
@@ -105,26 +102,25 @@ public class ConnectActivity extends NonHomeActivity {
         }
     }
 
-        @Override
-        protected void onResume()
-        {     //Set adapter onResume, so that our list updates every time we come to the screen,
-            super.onResume();           //not just the first time
-            ArrayMap<String, Object> myInfoAttributes = Utilities.jsonToMap(App.databaseIoManager.getContact(0).getAttributes());
-            recyclerView.setAdapter(new ConnectAdapter(myInfoAttributes));
-            setupForegroundDispatch(this, nfcAdapter); // Start the foreground dispatch so that the intent can be caught
-        }
+    @Override
+    protected void onResume() {     //Set adapter onResume, so that our list updates every time we come to the screen,
+        super.onResume();           //not just the first time
+        ArrayMap<String, Object> myInfoAttributes = Utilities.jsonToMap(App.databaseIoManager.getContact(0).getAttributes());
+        recyclerView.setAdapter(new ConnectAdapter(myInfoAttributes));
+        setupForegroundDispatch(this, nfcAdapter); // Start the foreground dispatch so that the intent can be caught
+    }
 
-        //adds the home button to the toolbar
-        @Override
-        public boolean onCreateOptionsMenu (Menu menu){
-            return super.onCreateOptionsMenu(menu);
-        }
+    //adds the home button to the toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
 
-        //home button takes you straight home, resets the list of activities for the back button
-        @Override
-        public boolean onOptionsItemSelected (MenuItem item){
-            return super.onOptionsItemSelected(item);
-        }
+    //home button takes you straight home, resets the list of activities for the back button
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 
     public void nfcCheck() {
         // This gets the manager object instantiated, and sets it to the NFC service
@@ -137,10 +133,10 @@ public class ConnectActivity extends NonHomeActivity {
             *  if adapter is null, then the device does not support NFC, however I felt that it was unnecessary to make it a separate elseif statement
             *  since it is included in the android manifest "uses Feature"
             */
-            Toast.makeText(getApplicationContext(), "NFC is on, you're good to go!",
+            makeText(getApplicationContext(), "NFC is on, you're good to go!",
                     Toast.LENGTH_SHORT).show(); //Show toast that NFC is on
         } else {
-            Toast.makeText(getApplicationContext(), "NFC must be turned on by User in Settings",
+            makeText(getApplicationContext(), "NFC must be turned on by User in Settings",
                     Toast.LENGTH_LONG).show(); //Show toast that NFC is off
             startActivityForResult(new Intent(android.provider.Settings.ACTION_NFC_SETTINGS), 23); //open settings page for NFC
         }
@@ -150,31 +146,27 @@ public class ConnectActivity extends NonHomeActivity {
         nfcAdapter = NfcAdapter.getDefaultAdapter(this); //Get the NFC adapter
 
         // Check whether NFC is enabled on device
-        if(!nfcAdapter.isEnabled()){
+        if (!nfcAdapter.isEnabled()) {
             // NFC is disabled, show the settings UI to enable NFC
-            Toast.makeText(this, "Please enable NFC.",
+            makeText(this, "Please enable NFC.",
                     Toast.LENGTH_SHORT).show();
             startActivity(new Intent(Settings.ACTION_NFC_SETTINGS));
-        }
-        // Check whether Android Beam feature is enabled on device
-        else if(!nfcAdapter.isNdefPushEnabled()) {
+        } else if (!nfcAdapter.isNdefPushEnabled()) {
+            // check if nfc adapter is enabled
             // Android Beam is disabled, show the settings UI to enable Android Beam
-            Toast.makeText(this, "Please enable Android Beam.",
+            makeText(this, "Please enable Android Beam.",
                     Toast.LENGTH_SHORT).show();
             startActivity(new Intent(Settings.ACTION_NFCSHARING_SETTINGS));
-        }
-        else {
+        } else {
             // NFC and Android Beam both are enabled
 
-            nfcAdapter.setNdefPushMessageCallback(new NfcAdapter.CreateNdefMessageCallback()
-            {
+            nfcAdapter.setNdefPushMessageCallback(new NfcAdapter.CreateNdefMessageCallback() {
 
                 //Creates an NdefMessage object to send to the other device, and waits for the device to appear
                 @Override
-                public NdefMessage createNdefMessage(NfcEvent event)
-                {
+                public NdefMessage createNdefMessage(NfcEvent event) {
                     NdefRecord uriRecord = NdefRecord.createUri(Uri.encode("http://www.google.com/"));
-                    return new NdefMessage(new NdefRecord[] { uriRecord });
+                    return new NdefMessage(new NdefRecord[]{uriRecord});
                 }
 
             }, this, this);
@@ -208,15 +200,13 @@ public class ConnectActivity extends NonHomeActivity {
         final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
 
         IntentFilter[] filters = new IntentFilter[1];
-        String[][] techList = new String[][]{};
-
-
         filters[0] = new IntentFilter();
         filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
         filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        String[][] techList = new String[][]{};
         try {
             filters[0].addDataType(MIME_TEXT_PLAIN);
-        } catch (IntentFilter.MalformedMimeTypeException e) {
+        } catch (IntentFilter.MalformedMimeTypeException exception) {
             throw new RuntimeException("Check your mime type.");
         }
 
@@ -229,59 +219,5 @@ public class ConnectActivity extends NonHomeActivity {
      */
     public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
         adapter.disableForegroundDispatch(activity);
-    }
-}
-    // NdefReaderTask is the task that takes the tag and ensures it is in fact and Ndef Message, and if so, pulles out the Ndef Record and passes that to the reader
-    class NdefReaderTask extends AsyncTask<Tag, Void, String> {
-
-    @Override
-    protected String doInBackground(Tag... params) {
-        Tag tag = params[0];
-
-        Ndef ndef = Ndef.get(tag);
-        if (ndef == null) {
-            // NDEF is not supported by this Tag.
-            return null;
-        }
-
-        NdefMessage ndefMessage = ndef.getCachedNdefMessage();
-
-        NdefRecord[] records = ndefMessage.getRecords(); //this allows more than one record per message, though our app does not need this.
-        for (NdefRecord ndefRecord : records) {
-            if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
-                try {
-                    return readText(ndefRecord);
-                } catch (UnsupportedEncodingException e) {
-                    Log.e(TAG, "Unsupported Encoding", e);
-                }
-            }
-        }
-
-        return null;
-    }
-    // Read Text reads the string within the Ndef Record and returns it as a string
-    private String readText(NdefRecord record) throws UnsupportedEncodingException {
-
-
-        byte[] payload = record.getPayload();
-
-        // Get the Text Encoding
-        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
-
-        // Get the Language Code
-        int languageCodeLength = payload[0] & 0063;
-
-        // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
-        // e.g. "en"
-
-        // Get the Text
-        return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
-    }
-
-    @Override
-    protected void onPostExecute(String result) {
-        if (result != null) {
-            //unclear at this time what needs to go in here
-        }
     }
 }
